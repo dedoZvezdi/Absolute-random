fc8c7544769fd24649143ec686af13c14ce53857
Svetlin Ivanov <115867744+dedoZvezdi@users.noreply.github.com>
2024-12-24 17:01:23 +0200

    More tests
    Minor chanes on some funtions and added more funtion for more test. Now i have 6 tests files after code run so I also added and folder to hold them all.

--- Best fodler EVER/Test/test/RNG/RNG.cpp
+++ Best fodler EVER/Test/test/RNG/RNG.cpp
@@ -1,5 +1,6 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 
+#include <direct.h>
 #include <iostream>
 #include <fstream>
 #include <ctime>
@@ -17,6 +18,12 @@ struct AverageData {
     int totalTests;
 };
 
+void static createDirectory(const string& path) {
+    if (_mkdir(path.c_str()) != 0 && errno != EEXIST) {
+        cerr << "Error creating directory: " << path << endl;
+    }
+}
+
 void static swapElements(int arr[], int size) {
     int firstElement = arr[0];
     for (int i = 0; i < size - 1; i++) {
@@ -29,7 +36,8 @@ void static swapElements(int arr[], int size) {
 
 map<int, AverageData> static readAverageHistory() {
     map<int, AverageData> history;
-    ifstream file("cumulative_averages.txt");
+    createDirectory("tests_results");
+    ifstream file("tests_results/cumulative_averages.txt");
     if (file.is_open()) {
         string line;
         while (getline(file, line)) {
@@ -46,7 +54,8 @@ map<int, AverageData> static readAverageHistory() {
 }
 
 void static saveCumulativeAverage(const map<int, AverageData>& averageHistory) {
-    ofstream file("cumulative_averages.txt");
+    createDirectory("tests_results");
+    ofstream file("tests_results/cumulative_averages.txt");
     if (file.is_open()) {
         for (const auto& pair : averageHistory) {
             file << pair.first << " swaps: " << pair.second.averageTime << " seconds (averaged "
@@ -55,31 +64,30 @@ void static saveCumulativeAverage(const map<int, AverageData>& averageHistory) {
         file.close();
     }
     else {
-        cerr << "Error1" << endl;
+        cerr << "Error creating cumulative_averages.txt" << endl;
     }
 }
 
 void static saveComparisonLog(const string& text, int logNumber) {
-    ofstream file("performance_comparison.txt", ios::app);
+    createDirectory("tests_results");
+    ofstream file("tests_results/performance_comparison.txt", ios::app);
     if (file.is_open()) {
         if (logNumber == 1) {
             file << "LOG 1 (Base, no comparison)\n";
-        }
-        else {
+        } else {
             file << "LOG " << logNumber << " (Compared to LOG " << logNumber - 1 << "):\n";
         }
         file << text;
         file.close();
-    }
-    else {
-        cerr << "Error2" << endl;
+    } else {
+        cerr << "Error writing to performance_comparison.txt" << endl;
     }
 }
 
 
 map<int, double> static readLastRun() {
     map<int, double> lastRun;
-    ifstream file("last_run.txt");
+    ifstream file("tests_results/last_run.txt");
     if (file.is_open()) {
         string line;
         while (getline(file, line)) {
@@ -95,7 +103,8 @@ map<int, double> static readLastRun() {
 }
 
 void static saveLastRun(const map<int, double>& currentRun) {
-    ofstream file("last_run.txt");
+    createDirectory("tests_results");
+    ofstream file("tests_results/last_run.txt");
     if (file.is_open()) {
         for (const auto& pair : currentRun) {
             file << pair.first << " swaps: " << pair.second << endl;
@@ -119,16 +128,73 @@ double static measureSwaps(int arr[], int size, int swaps, int tests) {
 }
 
 void static saveRunHistory(const map<int, double>& currentRun, int runNumber) {
-    ofstream file("run_history.txt", ios::app);
+    createDirectory("tests_results");
+    ofstream file("tests_results/run_history.txt", ios::app);
     if (file.is_open()) {
         file << "Run " << runNumber << ":\n";
         for (const auto& pair : currentRun) {
             file << pair.first << " swaps: " << pair.second << " seconds\n";
         }
         file.close();
+    } else {
+        cerr << "Error writing to run_history.txt" << endl;
     }
-    else {
-        cerr << "Error3" << endl;
+}
+
+void static saveMaxDifference(const map<int, double>& currentRun, const map<int, double>& lastRun, int runNumber) {
+    createDirectory("tests_results");
+    ofstream file("tests_results/max_difference_log.txt", ios::app);
+    if (file.is_open()) {
+        file << "Run " << runNumber << ": ";
+        if (runNumber == 1) {
+            file << "Base (no data to compare)\n";
+        } else {
+            int maxSwaps = 0;
+            double maxDifference = 0.0;
+            double timeRun1 = 0.0;
+            double timeRun2 = 0.0;
+
+            for (const auto& pair : currentRun) {
+                int swaps = pair.first;
+                double currentTime = pair.second;
+                double lastTime = lastRun.at(swaps);
+                double difference = abs(currentTime - lastTime);
+                if (difference > maxDifference) {
+                    maxDifference = difference;
+                    maxSwaps = swaps;
+                    timeRun1 = lastTime;
+                    timeRun2 = currentTime;
+                }
+            }
+            file << "Swaps " << maxSwaps << " time_run" << runNumber - 1 << " (" << timeRun1 << " s) -> time_run" << runNumber
+                 << " (" << timeRun2 << " s) = " << maxDifference << " seconds\n";
+        }
+        file.close();
+    } else {
+        cerr << "Error writing to max_difference_log.txt" << endl;
+    }
+}
+
+void static saveTotalRunTime(const map<int, double>& currentRun, int tests, int runNumber) {
+    createDirectory("tests_results");
+    ofstream file("tests_results/total_run_time.txt", ios::app);
+    if (file.is_open()) {
+        double totalRunTime = 0.0;
+        double firstTestTime = 0.0;
+        bool firstTest = true;
+
+        for (const auto& pair : currentRun) {
+            totalRunTime += pair.second * tests;
+            if (firstTest) {
+                firstTestTime = pair.second * tests;
+                firstTest = false;
+            }
+        }
+        file << "Run " << runNumber << ": up to " << currentRun.size() << " swaps and " << tests << " tests per swap -> total time: "
+             << totalRunTime << " seconds, first swap test time: " << firstTestTime << " seconds\n";
+        file.close();
+    } else {
+        cerr << "Error writing to total_run_time.txt" << endl;
     }
 }
 
@@ -186,6 +252,9 @@ int main() {
     saveComparisonLog(comparisonResult.str(), runNumber);
     saveLastRun(currentRun);
     saveRunHistory(currentRun, runNumber);
+
+    saveMaxDifference(currentRun, lastRun, runNumber);
+    saveTotalRunTime(currentRun, tests, runNumber);
 
     return 0;
 }
